ac8b48a0c77ee8f6ea91ee4501f2bfe256852459
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-10-11 16:18:10 -0300

    added Ex9b

--- Haskell/Activity1/Exercises1/Ex9b.hs
+++ Haskell/Activity1/Exercises1/Ex9b.hs
@@ -1,4 +1,4 @@
--- Exercício 09: Encontre os 10 primeiros anos bissextos.
+-- Exercício 09b: Encontre os 10 últimos anos bissextos (dica: use a função length para determinar o tamanho da lista).
 -- Copyright (c) 2017 Daniel Gonçalves da Silva - https://github.com/danielgoncalvesti
 -- GPL version 3 or later (see http://www.gnu.org/copyleft/gpl.html)
 --
@@ -11,11 +11,9 @@ isLeap year
   | year `rem`   4 == 0 = True
   | otherwise           = False
   
-
 main :: IO()
 
 main = do
-    let years = [1..2017]
-    let list = [x | x <- years, isLeap x]
-    let list2 = take 10 list
-    print(list2)
+    let yearsReverse = reverse [1..2017]
+    let lista = take 10 [x | x <- yearsReverse, isLeap x]
+    print(lista)
