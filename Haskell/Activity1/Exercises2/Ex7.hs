e7b10b2dc74fe74a28b5d968b940f3d24d137394
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-10-16 10:09:57 -0200

    added Ex7.hs

--- Haskell/Activity1/Exercises2/Ex7.hs
+++ Haskell/Activity1/Exercises2/Ex7.hs
@@ -1,1 +1,10 @@
 -- Exercício 07: Faça uma função que calcule o coeficiente binomial de (m,n).
+binom :: Int -> Int -> Int
+binom = loop 1 1
+  where
+    loop rn rd _ 0 = rn `div` rd
+    loop _  _  0 _ = 0
+    loop rn rd n k = loop (rn * n) (rd * k) (n-1) (k-1)
+    
+main = do 
+    print(binom 10 6)
