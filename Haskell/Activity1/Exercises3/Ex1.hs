ed1b7615a37f326a1319ffcc0c6b25bfa9cc5ed2
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-10-16 11:54:08 -0200

    added Ex1.hs

--- Haskell/Activity1/Exercises3/Ex1.hs
+++ Haskell/Activity1/Exercises3/Ex1.hs
@@ -1,12 +1,14 @@
 -- Exercício 01: Crie uma função divisivel20 x que retorna verdadeiro se x for divisível por todos os números de 1 a 20.
-
-module Main where
-
 divisivel20 :: Integer -> Bool
-divisivel20 x 
-  | x 
+divisivel20 x = dividir x 20
 
-
+dividir :: Integer -> Integer -> Bool
+dividir x y
+       | y == 1 = True
+       | x `mod` y /= 0 = False
+       | otherwise = dividir x (y-1)
+       
 main :: IO()
 main = do
-     print("ola")
+     print(divisivel20 0) --True
+     print(divisivel20 20) -- False
