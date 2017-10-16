c13edea07cf1572a817c385104545c60f31eafa0
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-10-16 00:23:20 -0200

    added Ex6.hs

--- Haskell/Activity1/Exercises2/Ex6.hs
+++ Haskell/Activity1/Exercises2/Ex6.hs
@@ -1,18 +1,11 @@
 -- Exercício 06: Faça uma função que calcule a persistência aditiva de um número.
-import Data.Tuple (swap)
-import Data.List (unfoldr)
- 
-digSum :: Int -> Int -> Int
-digSum base = sum . unfoldr f
-  where
-    f 0 = Nothing
-    f n = Just (swap (quotRem n base))
- 
-digRoot :: Int -> Int -> (Int, Int)
-digRoot base =
-  head . dropWhile ((>= base) . snd) . zip [0 ..] . iterate (digSum base)
- 
-main :: IO ()
+additivePersistence :: Int -> Int
+additivePersistence x 
+            |x < 10 = 0
+            |otherwise = (additivePersistence (sum $ numSum x)) + 1
+            where 
+              numSum 0 = [0]
+              numSum x = numSum (x `div` 10) ++ [x `rem` 10]
+
 main = do
-  putStrLn "in base 10:"
-  mapM_ (print . ((,) <*> digRoot 10)) [22, 99, 111, 4556]
+    print (additivePersistence 111)
