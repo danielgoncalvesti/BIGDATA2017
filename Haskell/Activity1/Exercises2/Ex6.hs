89dd8c0e92a2d247eeb183e5945017b0aa0a6774
Daniel Gonçalves da Silva <danielgoncalvesti@gmail.com>
2017-10-15 23:30:22 -0200

    added a version to Ex6.hs
    This solution to Ex6 was got at the link: https://rosettacode.org/wiki/Digital_root#Haskell

--- Haskell/Activity1/Exercises2/Ex6.hs
+++ Haskell/Activity1/Exercises2/Ex6.hs
@@ -1,1 +1,18 @@
 -- Exercício 06: Faça uma função que calcule a persistência aditiva de um número.
+import Data.Tuple (swap)
+import Data.List (unfoldr)
+ 
+digSum :: Int -> Int -> Int
+digSum base = sum . unfoldr f
+  where
+    f 0 = Nothing
+    f n = Just (swap (quotRem n base))
+ 
+digRoot :: Int -> Int -> (Int, Int)
+digRoot base =
+  head . dropWhile ((>= base) . snd) . zip [0 ..] . iterate (digSum base)
+ 
+main :: IO ()
+main = do
+  putStrLn "in base 10:"
+  mapM_ (print . ((,) <*> digRoot 10)) [22, 99, 111, 4556]
